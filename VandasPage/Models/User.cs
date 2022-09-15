2d9a4c826b1950a3f627453c6856d8e88c9d5299
nagyD88 <oliverdikker@gmail.com>
2022-09-15 15:51:48 +0200

    Info of user Class

---
+++ VandasPage/Models/User.cs
@@ -0,0 +1,14 @@
+﻿namespace VandasPage.Models;
+
+public class User
+{
+    private string? _emial;
+    private string? _reasonForApplication;
+    private bool _directInquiry;
+    private string _communication;
+    private List<Questionnaire> _questionnaires;
+    private string MBTI;
+    private decimal price;
+    private int numberOfDetails;
+    private string meetingLog;
+}
