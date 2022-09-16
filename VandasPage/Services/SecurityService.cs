19db0cbf8084af29f898de9fff95af299e65d940
nagyD88 <oliverdikker@gmail.com>
2022-09-16 13:34:33 +0200

    securityService finds users in dataBase

--- VandasPage/Services/SecurityService.cs
+++ VandasPage/Services/SecurityService.cs
@@ -4,16 +4,13 @@ namespace VandasPage.Services
 {
     public class SecurityService
     {
-        List<User> users = new List<User>();
-
+        UsersDAO usersDAO = new UsersDAO();
         public SecurityService()
         {
-            users.Add(new User("Admin", "oliverdikker@gmail.com" ));
-            users.Add(new User("1234", "alma@alma.hu"));
         }
         public bool IsValid(User user) 
         {
-            return users.Any(x => x.Emial == user.Emial && x.Password== user.Password);
+            return usersDAO.FindUserByEmailAndPassword(user);
         }
     }
 }
