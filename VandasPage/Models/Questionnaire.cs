619fc8a6b5839f05be4d840f85fbe882ff65f898
nagyD88 <oliverdikker@gmail.com>
2022-09-16 07:41:48 +0200

    create questionaire with general functions

---
+++ VandasPage/Models/Questionnaire.cs
@@ -0,0 +1,27 @@
+﻿namespace VandasPage.Models;
+
+public class Questionnaire
+{
+
+    private List<Question> _questions;
+    
+    public int GetAllPoint()
+    {
+        throw new NotImplementedException();
+    }
+
+    public int GetGeneralPresentPoints()
+    {
+        throw new NotImplementedException();
+    }
+
+    public int GetFamilyPoints()
+    {
+        throw new NotImplementedException();
+    }
+
+    public int GetPerformancePoints()
+    {
+        throw new NotImplementedException();
+    }
+}
