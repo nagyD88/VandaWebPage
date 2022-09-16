560e0a2b1bf6c69a5ee578b7f118bdfa6f37bc1f
nagyD88 <oliverdikker@gmail.com>
2022-09-16 10:41:14 +0200

    user gets data for login

--- VandasPage/Models/User.cs
+++ VandasPage/Models/User.cs
@@ -1,14 +1,18 @@
-﻿namespace VandasPage.Models;
-
-public class User
+﻿namespace VandasPage.Models
 {
-    private string? _emial;
-    private string? _reasonForApplication;
-    private bool _directInquiry;
-    private string _communication;
-    private List<Questionnaire> _questionnaires;
-    private string MBTI;
-    private decimal price;
-    private int numberOfDetails;
-    private string meetingLog;
+
+    public class User
+    {
+        public int Id { get; set; }
+        public string password { get; set; }
+        public string Emial { get; set; }
+        private string _reasonForApplication;
+        private bool _directInquiry;
+        private string _communication;
+        private List<Questionnaire> _questionnaires;
+        private string MBTI;
+        private decimal price;
+        private int numberOfDetails;
+        private string meetingLog;
+    }
 }
