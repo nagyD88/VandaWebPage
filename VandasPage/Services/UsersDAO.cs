470af06a9a3783767f6ff07794d3a3ff3c80918f
nagyD88 <oliverdikker@gmail.com>
2022-09-16 13:24:53 +0200

    userDAO finds Userwhit email and password in database

---
+++ VandasPage/Services/UsersDAO.cs
@@ -0,0 +1,39 @@
+﻿using System.Data.SqlClient;
+using VandasPage.Models;
+
+namespace VandasPage.Services
+{
+    public class UsersDAO
+    {
+        string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=VandasWebDataBase;
+                                       Integrated Security=True;Connect Timeout=30;Encrypt=False;
+                                        TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        public bool FindUserByNameAndPassword(User user)
+        {
+            bool success=false;
+            string SQLstatment="SELECT * FROM dbo.Users WHERE EMAIL= @email AND PASSWORD= @password"
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(SQLstatment, connection);
+                command.Parameters.Add("@email", System.Data.SqlDbType.Text).Value=user.Email;
+                command.Parameters.Add("@password", System.Data.SqlDbType.VarChar,40).Value = user.Password;
+
+
+                try
+                {
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+                    if (reader.HasRows)
+                    {
+                        success = true;
+                    }
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            } 
+            return success;
+        }
+    }
+}
