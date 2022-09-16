8ef087b5922f465a6b7879dc92e3d4e4415a67a2
nagyD88 <oliverdikker@gmail.com>
2022-09-16 15:38:10 +0200

    registrationcontroller works

---
+++ VandasPage/Controllers/RegistrationController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VandasPage.Models;
+using VandasPage.Services;
+
+namespace VandasPage.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public void RegistrationProcess(User user)//viszajelzés még kell
+        {
+            UsersDAO usersDAO = new UsersDAO();
+            usersDAO.RegisterNewUser(user);
+        }
+    }
+}
