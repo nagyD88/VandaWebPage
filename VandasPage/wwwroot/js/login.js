48a47279b05470eaeb6dbdc5b5c74463b62e7d5c
nagyD88 <oliverdikker@gmail.com>
2022-09-16 10:00:31 +0200

    start of login in

---
+++ VandasPage/wwwroot/js/login.js
@@ -0,0 +1,13 @@
+﻿const login={
+    getLoginData:function(){
+        let email = document.querySelector("#emailInput").value;
+        let password = document.querySelector("#passwordInput").value;
+        
+        // make something like this!
+        
+        /*dataHandler.logUserIn(userName, password).then((data) => {
+            if (data["result"] === "successful") {
+                domManager.switchToLoggedIn(logOutHandler, userName);
+                domManager.refreshPage();*/
+    }
+}
