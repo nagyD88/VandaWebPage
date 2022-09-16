c2e916aaa5f140c8c483a210960638397c2a0d29
nagyD88 <oliverdikker@gmail.com>
2022-09-16 10:07:23 +0200

    create dataHandler.js with login part

---
+++ VandasPage/wwwroot/js/DataHandler.js
@@ -0,0 +1,33 @@
+﻿
+export let dataHandler={
+    logUserIn: async function (userName, password) {
+        return await apiPost('api/users/login', {
+            'user_name': userName,
+            'password': password
+        });
+    },
+    logUserOut: async function() {
+        await apiGet('api/users/logout')
+    }
+}
+
+async function apiGet(url) {
+    let response = await fetch(url, {
+        method: "GET",
+    });
+    if (response.ok) {
+        return await response.json();
+    }
+}
+
+async function apiPost(url, payload) {
+    const response = await fetch(url, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(payload)
+    });
+    return await response.json();
+}
+
