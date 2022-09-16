719354c185206cbfdffc273f8ac5b7dab7a7bf33
nagyD88 <oliverdikker@gmail.com>
2022-09-16 11:53:37 +0200

    create an agregator class

---
+++ VandasPage/Services/SecurityService.cs
@@ -0,0 +1,19 @@
+﻿using VandasPage.Models;
+
+namespace VandasPage.Services
+{
+    public class SecurityService
+    {
+        List<User> users = new List<User>();
+
+        public SecurityService()
+        {
+            users.Add(new User("Admin", "oliverdikker@gmail.com" ));
+            users.Add(new User("1234", "alma@alma.hu"));
+        }
+        public bool IsValid(User user) 
+        {
+            return users.Any(x => x.Emial == user.Emial && x.Password== user.Password);
+        }
+    }
+}
