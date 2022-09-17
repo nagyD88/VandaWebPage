fcd0636b9110af77ab7f027fdbc2f5339a3e3b0a
nagyD88 <oliverdikker@gmail.com>
2022-09-17 11:32:27 +0200

    create admin functions

--- VandasPage/wwwroot/js/site.js
+++ VandasPage/wwwroot/js/site.js
@@ -1,9 +1,22 @@
-﻿
-let ToLogin=function(){
-    if(window.sessionStorage.length===0){
-        window.location.replace("/Login");
+﻿import {registration} from "./HtmlFactory.js";
+
+export let admin = {
+    isAdmin: function () {
+        return window.sessionStorage["admin"] === "true";
+    },
+
+    registration: function () {
+        if (this.isAdmin()) {
+            
+            registration.createRegistrationButton();
+        }
     }
 }
 
-
+let ToLogin = function () {
+    if (window.sessionStorage.length === 0) {
+        window.location.replace("/Login");
+    }
+}
 ToLogin();
+admin.registration();
