cede58a0f40f8a161ff68cd70f7e1d5e993c59e6
nagyD88 <oliverdikker@gmail.com>
2022-09-17 17:10:30 +0200

    create Registration.js

---
+++ VandasPage/wwwroot/js/Registration.js
@@ -0,0 +1,14 @@
+﻿import {dataHandler} from "./DataHandler.js";
+
+
+export const registration={
+    isRegistration:function(){
+        let email=document.getElementById("emailInput").value;
+        let password=document.getElementById("passwordInput").value;
+        let admin=document.getElementById("adminInput").checked;
+        dataHandler.registration(email,password,admin).then(data=>console.log(data));
+    }
+    
+}
+
+
