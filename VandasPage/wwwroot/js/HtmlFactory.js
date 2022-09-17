f2f64cff12a05c83a9131ff1f0fafc47906d3b3b
nagyD88 <oliverdikker@gmail.com>
2022-09-17 14:42:52 +0200

    html factory maks html easier way

--- VandasPage/wwwroot/js/HtmlFactory.js
+++ VandasPage/wwwroot/js/HtmlFactory.js
@@ -1,3 +1,26 @@
+let RegForm = ` <div class="row">
+                    <div class="col-md-4">
+                        <form action="/action_page.php">
+                            <div class="form-group">
+                                <label class="control-label" for="email">e-mail:</label><br>
+                                <input class="form-control" type="text" id="emailInput" name="email">
+                            </div>
+                            <div class="form-group">
+                                <label class="control-label" for="lname">password:</label>
+                                <input class="form-control" type="password" id="passwordInput" name="password"><br>
+                            </div>
+                            <div class="form-group form-check">
+                                <label for="admin">
+                                    <input class="form-check-input" type="checkbox" id="adminInput" name="admin" value="admin">
+                                Admin</label><br><br>
+                            </div>
+                            <div class="form-group">
+                                <input id="registerButton" class="btn btn-primary" value=Registration>
+                             </div>
+                        </form>
+                    </div> 
+                </div>`;
+
 export const registration = {
     createRegistrationButton: function () {
         let container = document.querySelector("#UpperButtonContainer");
@@ -7,53 +30,16 @@ export const registration = {
         let regButton = document.createElement("a");
         li.appendChild(regButton);
         regButton.setAttribute("class", "nav-link text-dark");
-        regButton.setAttribute("href", "/Registration");
+        regButton.setAttribute("href", "");
         regButton.innerText = "registration";
+        regButton.addEventListener("click", (event) => {
+            event.preventDefault()
+            this.createRegistrationForm();
+            console.log("ker");
+        });
     },
     createRegistrationForm: function () {
         let container = document.querySelector("#mainContainer");
-        let div = document.createElement("div");
-        let Form = document.createElement("form");
-        container.appendChild(div);
-        div.appendChild(Form);
-        for (let i = 0; i < 2; i++) {
-            let div1 = document.createElement("div");
-            Form.appendChild(div1);
-            div1.setAttribute("class", "form-group");
-            let label = document.createElement("label");
-            div1.appendChild(label);
-            label.setAttribute("class", "control-label");
-            let input = document.createElement("input");
-            div1.appendChild(input);
-            input.setAttribute("class", "form-control");
-            input.setAttribute("type", "text");
-        }
-        let passwordInput=Form.getElementsByTagName("input")[0];
-        passwordInput.setAttribute("id", "passwordInput");
-        let emailInput=Form.getElementsByTagName("input")[1];
-        emailInput.setAttribute("id", "emailInput");
-        let passwordLabel= Form.getElementsByTagName("label")[0]
-        passwordLabel.innerText="Password";
-        let emailLabel=Form.getElementsByTagName("label")[1]
-        let adminDiv= document.createElement("div")
-        Form.appendChild(adminDiv);
-        adminDiv.setAttribute("class", "form-check-label");
-        let adminInput= document.createElement("input");
-        adminInput.setAttribute("class", "form-check-input");
-        adminInput.setAttribute("type", "checkbox")
-        let adminLabel = document.createElement("label");
-        adminLabel.setAttribute("class","form-check-input");
-        adminDiv.appendChild(adminLabel);
-        adminLabel.appendChild(adminInput);
-        adminInput.innerText="Admin";
-        let buttonDiv= document.createElement("div");
-        buttonDiv.setAttribute("class","form-group" )
-        let button= document.createElement("input");
-        button.innerText="registration";
-        Form.appendChild(buttonDiv);
-        buttonDiv.appendChild(button);
-        button.setAttribute("class","btn btn-primary");
-        
-        
+        container.innerHTML = RegForm;
     }
 }
