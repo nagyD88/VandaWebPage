4def16fc5206c748fa397827f6098f91171ff571
nagyD88 <oliverdikker@gmail.com>
2022-09-18 17:09:12 +0200

    create GetAllSettableDataByID method

--- VandasPage/Controllers/DataController.cs
+++ VandasPage/Controllers/DataController.cs
@@ -15,4 +15,13 @@ public class DataController : Controller
         string serializeObject = Newtonsoft.Json.JsonConvert.SerializeObject(users);
         return serializeObject;
     }
+
+    [Route("/Data/GetAllSettableDataByID/{ID}")]
+    public string GetAllSettableDataByID(string ID)
+    {
+        int Id = Int32.Parse(ID);
+        User user = usersDao.FindUserByID(Id);
+        string serializeObject = Newtonsoft.Json.JsonConvert.SerializeObject(user);
+        return serializeObject;
+    }
 }
