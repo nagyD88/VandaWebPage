587141e4750764b34738b2526c9103f5a663d20b
nagyD88 <oliverdikker@gmail.com>
2022-09-30 10:16:48 +0200

    create UpdateController.cs

---
+++ VandasPage/Controllers/UpdateController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VandasPage.Models;
+using VandasPage.Services;
+
+namespace VandasPage.Controllers;
+
+public class UpdateController : Controller
+{
+    [HttpPut]
+    [Route("update/ProcessUpdate")]
+    public string RegistrationProcess([FromBody] User user)
+    {
+        UsersDAO usersDAO = new UsersDAO();
+        bool success =usersDAO.UpdateUser(user);
+        string serializeObject = Newtonsoft.Json.JsonConvert.SerializeObject(success);
+        return serializeObject;
+    }
+}
