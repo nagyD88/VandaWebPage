cf9307c249e4890f0cd4428b6c8a3c5775b53319
nagyD88 <oliverdikker@gmail.com>
2022-11-22 12:57:31 +0100

    create basic context

---
+++ VandasPage/Data/VandaContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VandasPage.Models;
+
+namespace VandasPage.Data
+{
+    public class VandaContext : DbContext
+    {
+        public VandaContext(DbContextOptions<VandaContext> options) : base(options)
+        {
+        }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Question> Questions { get; set; }
+        public DbSet<Questionnaire> Questionnaires { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().ToTable("users");
+            modelBuilder.Entity<Question>().ToTable("questions");
+            modelBuilder.Entity<Questionnaire>().ToTable("questionnaires");
+        }
+    }
+}
+
