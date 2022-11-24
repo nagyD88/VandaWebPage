59baf892b9571738779d04e9b628b8558442a0f1
nagyD88 <oliverdikker@gmail.com>
2022-11-24 14:40:04 +0100

    darkmod in navbar

--- VandasPage/frontend/src/Nav.js
+++ VandasPage/frontend/src/Nav.js
@@ -1,21 +1,24 @@
 import { Link } from 'react-router-dom';
-const Nav = () => {
+import { useContext } from 'react';
+import DataContext from './dataContext/dataContext';
 
+const Nav = () => {
+  const { colorTheme } = useContext(DataContext);
 return (
-    <nav className="sideNav">
-      <div className="navElement active">
+    <nav className={`sideNav ${colorTheme}`} >
+      <div className={`navElement ${colorTheme}`}>
         <Link to="/">Home</Link>
       </div>
-      <div className="navElement">
+      <div className={`navElement ${colorTheme}`}>
         <Link to="register">Regisztráció</Link>
       </div>
-      <div className="navElement">
+      <div className={`navElement ${colorTheme}`}>
         <Link to="User">Felhasználók</Link>
       </div>
-      <div className="navElement">
+      <div className={`navElement ${colorTheme}`}>
         <Link to="questionnaire">Kérdőív</Link>
       </div>
-      <div className="navElement">
+      <div className={`navElement ${colorTheme}`}>
         <Link to="Education">Oktató felület</Link>
       </div>
     </nav>
