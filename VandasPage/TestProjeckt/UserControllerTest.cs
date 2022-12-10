3e2e4eec905ee02eabf88cd7a6b59757832ac55d
nagyD88 <oliverdikker@gmail.com>
2022-12-10 15:59:12 +0100

    rename test file

---
+++ VandasPage/TestProjeckt/UserControllerTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestProjeckt
+{
+    [TestClass]
+    public class UserControllerTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
