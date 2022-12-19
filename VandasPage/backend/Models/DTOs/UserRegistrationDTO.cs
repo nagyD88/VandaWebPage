e3f5083d6e569aeea07e261d785d6fc10df23760
nagyD88 <oliverdikker@gmail.com>
2022-12-19 13:49:20 +0100

    create userRegistrationDTO

---
+++ VandasPage/backend/Models/DTOs/UserRegistrationDTO.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace VandasPage.Models.DTOs
+{
+    public class UserRegistrationDTO
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long Id { get; set; }
+        [Required]
+        public string Email { get; set; }
+        public bool Admin { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+    }
+}
