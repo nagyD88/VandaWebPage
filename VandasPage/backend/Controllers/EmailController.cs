06b3b21547b3e68e10917ef4bd6a306214718c27
nagyD88 <oliverdikker@gmail.com>
2022-12-22 10:24:25 +0100

    create an email controller

---
+++ VandasPage/backend/Controllers/EmailController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MailKit.Net.Smtp;
+using MailKit.Security;
+using Microsoft.AspNetCore.Http;
+using MimeKit;
+using MimeKit.Text;
+
+namespace VandasPage.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmailController : ControllerBase
+    {
+        private readonly IEmailService _emailService;
+
+        public EmailController(IEmailService emailService)
+        {
+            _emailService = emailService;
+        }
+
+        [HttpPost]
+        public IActionResult SendEmail(EmailDto request)
+        {
+            _emailService.SendEmail(request);
+            return Ok();
+        }
+    }
+}
