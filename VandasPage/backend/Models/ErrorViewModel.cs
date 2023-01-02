8d221e7778bed5e68ee25ffb2355a26fc026629f
nagyD88 <oliverdikker@gmail.com>
2023-01-02 15:26:24 +0100

    delete unused parts

--- VandasPage/backend/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace VandasPage.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
