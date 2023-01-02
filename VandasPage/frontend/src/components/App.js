2bc768ec4e8c6d12e5afb38259093172f18507c4
nagyD88 <oliverdikker@gmail.com>
2023-01-02 10:40:49 +0100

    create some protected roots

--- VandasPage/frontend/src/components/App.js
+++ VandasPage/frontend/src/components/App.js
@@ -10,34 +10,43 @@ import Registration from './Registration';
 import PreRegistration from './PreRegistration';
 import Login from './Login';
 import Missing from './Missing';
+import Unauthorized from './Unauthorized';
 import { Route, Routes } from 'react-router-dom';
 import { DataProvider } from '../context/dataContext';
 import { AuthProvider } from '../context/AuthProvider';
+import RequireAuth from './RequireAuth';
 
 function App() {
   return (
     <AuthProvider>
-    <DataProvider>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="User">
-            <Route index element={<Users />} />
-            <Route path=":id" element={<User />} />
+      <DataProvider>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            {/* public routes */}
+            <Route path="Login" element={<Login />} />
+            <Route path="unauthorized" element={<Unauthorized />} />
+            <Route path="*" element={<Missing />} />
+            {/* we want to protect these routes */}
+            <Route
+              element={<RequireAuth allowedRoles={[ROLES.User]} />}
+            >
+              <Route index element={<Home />} />
+              <Route path="User">
+                <Route index element={<Users />} />
+                <Route path=":id" element={<User />} />
+              </Route>
+              <Route path="questionnaire">
+                <Route index element={<Questionarys />} />
+                <Route path=":id" element={<Questionary />} />
+              </Route>
+              <Route path="Education">
+                <Route index element={<Education />} />
+              </Route>
+              <Route path="register" element={<PreRegistration />} />
+            </Route>
           </Route>
-          <Route path="questionnaire">
-            <Route index element={<Questionarys />} />
-            <Route path=":id" element={<Questionary />} />
-          </Route>
-          <Route path="Education">
-            <Route index element={<Education />} />
-          </Route>
-          <Route path="register" element={<PreRegistration />} />
-          <Route path="Login" element={<Login />} />
-          <Route path="*" element={<Missing />} />
-        </Route>
-      </Routes>
-    </DataProvider>
+        </Routes>
+      </DataProvider>
     </AuthProvider>
   );
 }
