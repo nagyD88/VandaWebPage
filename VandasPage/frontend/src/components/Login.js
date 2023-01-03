386748ee072d1a539e96689a438e8206b9cb56be
nagyD88 <oliverdikker@gmail.com>
2023-01-03 15:58:05 +0100

    login with protected rules works

--- VandasPage/frontend/src/components/Login.js
+++ VandasPage/frontend/src/components/Login.js
@@ -39,9 +39,9 @@ const Login = () => {
 
       console.log(JSON.stringify(response?.data));
       //console.log(JSON.stringify(response));
-      const accessToken = response?.data?.accessToken;
-      const roles = response?.data?.roles;
-      setAuth({ user: email, pwd: password, roles, accessToken });
+      //const accessToken = response?.data?.accessToken;
+      const roles = response?.data?.role;
+      setAuth({ user: email, pwd: password, roles/*, accessToken*/ });
       setUser('');
       setPwd('');
       navigate(from, { replace: true });
