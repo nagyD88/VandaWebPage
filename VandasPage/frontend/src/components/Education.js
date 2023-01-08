a91ca2c813ec06b30700966f4409f737f5bd7e4a
nagyD88 <oliverdikker@gmail.com>
2023-01-08 16:40:19 +0100

    gets all the levels and the category names

--- VandasPage/frontend/src/components/Education.js
+++ VandasPage/frontend/src/components/Education.js
@@ -1,20 +1,27 @@
 import React from 'react';
 import useAxiosFetch from '../hooks/useAxiosFetch';
 import dataContext from '../context/dataContext';
-import { useContext } from 'react';
-import EducationMaterial from './EducationMaterial';
+import { useContext, useEffect } from 'react';
+import Level from './Level';
 const Education = () => {
-  let url = 'https://localhost:7168/api/education';
+  let url = 'https://localhost:7168/api/Education/level';
   const { data, fetchError, isLoading } = useAxiosFetch(url);
   const { colorTheme } = useContext(dataContext);
   console.log(data);
 
+  const categorys = new Set();
+
+  useEffect(() => {
+    data.map((level) => categorys.add(level.categoryName));
+    console.log("category")
+    console.log(categorys);
+  }, [data]);
   return (
     <>
       <div>Education</div>
-      {data.map(material => (
-                <EducationMaterial key={material.id} material={material} />
-            ))}
+      {data.map((level) => (
+        <Level key={level.id} level={level} />
+      ))}
     </>
   );
 };
