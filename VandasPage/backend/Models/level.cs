04a04137b6483c3d1adefb6327d4caf3bd2aac5f
nagyD88 <oliverdikker@gmail.com>
2023-01-08 14:26:01 +0100

    level gets own name

--- VandasPage/backend/Models/level.cs
+++ VandasPage/backend/Models/level.cs
@@ -7,6 +7,7 @@ namespace VandasPage.Models
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public long Id { get; set; }
         public string CategoryName { get; set; }
+        public string Name { get; set; }
         public int LevelNumber { get; set; }
         public HashSet<User> users { get; set; }
         public HashSet<EducationalMaterial> educationalMaterials { get; set; }
