4ba35a8debc8b31068f408a946db11d23eff1fe3
nagyD88 <oliverdikker@gmail.com>
2023-01-08 13:10:18 +0100

    create page for all the materials

---
+++ VandasPage/frontend/src/components/EducationMaterial.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const EducationMaterial = ({ material }) => {
+  return (
+    <div className="education-material">
+      <h2>{material.name}</h2>
+      {material.type == 'text' && (
+        <article>{material.content}</article>
+      )}
+      {material.type == 'picture' && (
+        <img src={material.content}></img>
+      )}
+      {material.type == 'video' && (
+        <div class="ratio ratio-16x9">
+          <iframe
+            src={material.content}
+            title="YouTube video"
+            allowFullScreen
+          ></iframe>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default EducationMaterial;
