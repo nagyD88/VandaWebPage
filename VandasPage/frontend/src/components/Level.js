c532d7e668fe475091cd029d51c9f1f23eb3ae42
nagyD88 <oliverdikker@gmail.com>
2023-01-14 19:01:58 +0100

    levle gets color theme for later use

--- VandasPage/frontend/src/components/Level.js
+++ VandasPage/frontend/src/components/Level.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import EducationMaterial from './EducationMaterial';
+import DataContext from '../context/dataContext';
+import { useContext } from 'react';
 
-const Level = ({level}) => {
+const Level = ({ level }) => {
+  const { colorTheme } = useContext(DataContext);
+ 
+
 
   return (
     <>
-    <h2>{level.name}</h2>
+     
+      <h2>{level.name}</h2>
       {level.educationalMaterials?.map((material) => (
         <EducationMaterial key={material.id} material={material} />
       ))}
