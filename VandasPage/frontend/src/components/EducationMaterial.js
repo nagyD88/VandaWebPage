5e5154acc668c232e2110aeb9e555a8d8b7e07cb
nagyD88 <oliverdikker@gmail.com>
2023-01-15 16:30:26 +0100

    implementing delet modal

--- VandasPage/frontend/src/components/EducationMaterial.js
+++ VandasPage/frontend/src/components/EducationMaterial.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import { getId } from '../utils/EmbedLinkCreator';
-
+import Dashboard from './Dashboard';
+import AreYouSure from './AreYouSure';
+import api from '../hooks/api';
 
 const EducationMaterial = ({ material }) => {
-    return (
+  const handleOnClick = async () => {
+    const response = await api.delete(`/education/${material.id}`);
+    console.log(response);
+    
+  };
+
+  return (
     <div className="education-material">
       <h3>{material.name}</h3>
+      <Dashboard
+        children={
+          <AreYouSure
+            handleOnClick={handleOnClick}
+            messege={'Biztos le akarod törölni?'}
+          />
+        }
+      />
       {material.type == 'text' && (
         <article>{material.content}</article>
       )}
@@ -13,12 +29,17 @@ const EducationMaterial = ({ material }) => {
         <img src={material.content}></img>
       )}
       {material.type == 'video' && (
-        <div className="ratio ratio-16x9">
-          <iframe
-            src={"https://www.youtube.com/embed/"+getId(material.content)}
-            title="YouTube video"
-            allowFullScreen
-          ></iframe>
+        <div className="video">
+          <div className="ratio ratio-16x9">
+            <iframe
+              src={
+                'https://www.youtube.com/embed/' +
+                getId(material.content)
+              }
+              title="YouTube video"
+              allowFullScreen
+            ></iframe>
+          </div>
         </div>
       )}
     </div>
