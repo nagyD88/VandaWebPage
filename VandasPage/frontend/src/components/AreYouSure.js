2fabbde229b12509063056196fdc9e7e1ac687b5
nagyD88 <oliverdikker@gmail.com>
2023-01-15 15:09:29 +0100

    create are you sure js

---
+++ VandasPage/frontend/src/components/AreYouSure.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AreYouSure = () => {
+  return (
+    <>
+      <div id='sure'>Are you sure you want to join this team? </div>
+      <button className='regbutton'>yes</button>
+    </>
+  );
+};
+
+export default AreYouSure;
