94a60acc56c00ec7d76342145034fc14f8bbcc80
nagyD88 <oliverdikker@gmail.com>
2023-01-15 10:21:11 +0100

    education is a navbar and used like it

--- VandasPage/frontend/src/components/Education.js
+++ VandasPage/frontend/src/components/Education.js
@@ -2,30 +2,34 @@ import React from 'react';
 import useAxiosFetch from '../hooks/useAxiosFetch';
 import dataContext from '../context/dataContext';
 import { useContext, useEffect } from 'react';
-import Level from './Level';
-import AddEducationMaterial from './AddEducationMaterial';
-import AuthContext from '../context/AuthProvider';
+import { Link } from 'react-router-dom';
+
+
 
 const Education = () => {
   let url = 'https://localhost:7168/api/Education/level';
   const { data, fetchError, isLoading } = useAxiosFetch(url);
   const { colorTheme } = useContext(dataContext);
-  const { auth } = useContext(AuthContext);
+  
   console.log(data);
 
   const categorys = new Set();
 
   useEffect(() => {
     data.map((level) => categorys.add(level.categoryName));
-    console.log("category")
+    console.log('category');
     console.log(categorys);
   }, [data]);
   return (
     <>
-      <div>Education</div>
-      {auth.roles && <AddEducationMaterial/>}
       {data.map((level) => (
-        <Level key={level.id} level={level} />
+        <Link key={level.id} to={`/education/${level.id}`}>
+          <div className="level">
+            <p>
+              {level.name} 
+            </p>
+          </div>
+        </Link>
       ))}
     </>
   );
