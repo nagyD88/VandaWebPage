3efa97d81bb4a906f708a5f1ab20e4ca060ab567
nagyD88 <oliverdikker@gmail.com>
2023-01-17 08:59:54 +0100

    implement a drag and drop method without database

--- VandasPage/frontend/src/components/Level.js
+++ VandasPage/frontend/src/components/Level.js
@@ -8,33 +8,43 @@ import Education from './Education';
 import Dashboard from './Dashboard';
 import { useContext, useState, useEffect } from 'react';
 import api from '../hooks/api';
-
+import { useQuery, useMutation, useQueryClient } from 'react-query';
+import { DragDropContext, Draggable } from 'react-beautiful-dnd';
+import { StrictModeDroppable as Droppable } from '../utils/StrictModeDroppable';
+import useAxiosFetch from '../hooks/useAxiosFetch';
 
 const Level = () => {
   const { id } = useParams();
-  const dataUrl=`education/level/${id}`
+  const url= `https://localhost:7168/api/education/level/${id}`
+  
+  const dataUrl = `education/level/${id}`;
   const { colorTheme, counter } = useContext(DataContext);
+  const { data } = useAxiosFetch(url);
   
   const { auth } = useContext(AuthContext);
-  const [materialData, setMaterialData]=useState([]);
-  
+  const [materialData, setMaterialData] = useState(data||[]);
+  const [eduMaterials, setEduMaterials] = useState(data.educationalMaterials || [])
   const [fetchError, setFetchError] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
+  
+  
+
 
   useEffect(() => {
     let isMounted = true;
 
     const fetchData = async (url) => {
-      
       try {
         const response = await api.get(url);
         if (isMounted) {
           setMaterialData(response.data);
+          setEduMaterials(response.data.educationalMaterials);
         }
       } catch (err) {
         if (isMounted) {
           setFetchError(err.message);
           setMaterialData([]);
+          setEduMaterials([]);
         }
       } finally {
         isMounted && setIsLoading(false);
@@ -43,16 +53,43 @@ const Level = () => {
     fetchData(dataUrl);
     console.log(counter);
   }, [counter, id]);
-  
-  
+
+  const handleOnDragEnd = (result) => {
+    if (!result?.destination) return;
+    const Materials = Array.from(eduMaterials);
+    const [reorderedItem] = Materials.splice(result.source.index, 1);
+    Materials.splice(result.destination.index, 0, reorderedItem);
+    setEduMaterials(Materials);
+  };
+
   return (
     <>
-     {auth.roles && <Dashboard children={<AddEducationMaterial  levelID={id} />}/>}
-     <div><Education/></div>
+      {auth.roles && (
+        <Dashboard children={<AddEducationMaterial levelID={id} />} />
+      )}
+      <div>
+        <Education />
+      </div>
       <h2>{materialData.name}</h2>
-      {materialData.educationalMaterials?.map((material) => (
-        <EducationMaterial key={material.id} material={material} />
-      ))}
+      <DragDropContext onDragEnd={handleOnDragEnd}>
+        <Droppable droppableId="educationMaterials">
+          {(provided) => (
+            <section {...provided.droppableProps} ref={provided.innerRef} >
+              {eduMaterials?.map((material, index) => {
+                return (
+                  <Draggable key={material.id} draggableId={material.id.toString()} index={index} >
+                     {(provided) => (
+                      <article ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps} ref={provided.innerRef}>
+                        <EducationMaterial key={material.id} material={material} />
+                      </article>
+                      )}
+                  </Draggable>
+                )})}
+                {provided.placeholder}
+            </section>
+          )}
+        </Droppable>
+      </DragDropContext>
     </>
   );
 };
