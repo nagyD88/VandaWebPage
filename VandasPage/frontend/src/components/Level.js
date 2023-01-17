5b0e9b0562a6b0e7ebb231715bbe9963c4af4057
nagyD88 <oliverdikker@gmail.com>
2023-01-17 15:18:57 +0100

    delete unused import

--- VandasPage/frontend/src/components/Level.js
+++ VandasPage/frontend/src/components/Level.js
@@ -4,7 +4,6 @@ import DataContext from '../context/dataContext';
 import { useContext } from 'react';
 import useAxiosFetch from '../hooks/useAxiosFetch';
 import { useParams } from 'react-router';
-import AddEducationMaterial from './AddEducationMaterial';
 import AuthContext from '../context/AuthProvider';
 import Education from './Education';
 
