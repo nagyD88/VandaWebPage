4ec80c323c10889661b19ae66b76c6a0ac12cf79
nagyD88 <oliverdikker@gmail.com>
2023-01-18 09:38:46 +0100

    create index property

--- VandasPage/backend/Models/level.cs
+++ VandasPage/backend/Models/level.cs
@@ -9,6 +9,7 @@ namespace VandasPage.Models
         public string CategoryName { get; set; }
         public string Name { get; set; }
         public int LevelNumber { get; set; }
+        public int Index { get; set; }
         public HashSet<User> users { get; set; }
         public HashSet<EducationalMaterial> educationalMaterials { get; set; }
     }
