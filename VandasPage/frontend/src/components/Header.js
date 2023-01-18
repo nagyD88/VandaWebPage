f09ddefea303092efde3b9c866351aaac8c58124
Beelzseals <88729970+Beelzseals@users.noreply.github.com>
2023-01-18 23:58:54 +0100

    init lesson navbar

--- VandasPage/frontend/src/components/Header.js
+++ VandasPage/frontend/src/components/Header.js
@@ -5,6 +5,22 @@ const Header = () => {
   return (
     <div className='header-container'>
         <Nav />
+        <ul className='course-list'>
+          <li>
+            First course
+          </li>
+          <li>
+            Second course
+          </li>
+          <li>
+            Third course
+          </li>
+          <li>
+            Fourth course
+          </li>
+        </ul>
+        <button className='btn'>New course</button>
+        <button className='btn'>New lesson</button>
     </div>
   )
 }
