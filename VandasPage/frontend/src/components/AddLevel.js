34546fd395e16b692ab446b8e44338795a7eb8e7
nagyD88 <oliverdikker@gmail.com>
2023-01-18 11:47:43 +0100

    fix to use level withouth categoryname

--- VandasPage/frontend/src/components/AddLevel.js
+++ VandasPage/frontend/src/components/AddLevel.js
@@ -4,25 +4,19 @@ import api from '../hooks/api';
 import DataContext from '../context/dataContext';
 
 const AddLevel = ({ levelID, hideModal }) => {
-  const [type, setType] = useState('text');
-  const [categoryName, setCategoryName] = useState('');
   const [name, setName] = useState('');
   const { setCounter, counter } = useContext(DataContext);
 
-  const onValueChange = (e) => {
-    setType(e.target.value);
-  };
+
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     console.log({
-      categoryName: {categoryName},
       name: {name},
       users: [],
       educationalMaterials: []
     })
     const response = await api.post('/Education/level', {
-      categoryName: `${categoryName}`,
       name: `${name}`,
       users: [],
       educationalMaterials: []
@@ -46,16 +40,7 @@ const AddLevel = ({ levelID, hideModal }) => {
             />
           </label>
 
-          <label>
-            téma:
-            <input
-              id="categoryName"
-              type="text"
-              required
-              value={categoryName}
-              onChange={(e) => setCategoryName(e.target.value)}
-            />
-          </label>
+
         <input type="submit" value="feltölt" className="sub" />
       </form>
     </>
