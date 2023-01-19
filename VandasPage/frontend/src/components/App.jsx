4540c0a2569026222994dc14541b5b081d71b998
Beelzseals <88729970+Beelzseals@users.noreply.github.com>
2023-01-19 01:28:00 +0100

    -rename js files to jsx -add missing React import

--- VandasPage/frontend/src/components/App.jsx
+++ VandasPage/frontend/src/components/App.jsx
@@ -1,21 +1,22 @@
-import "./App.css";
+import React from 'react';
+import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Home from "./Home";
-import Layout from "./Layout";
-import Users from "./Users";
-import User from "./User";
-import Questionary from "./Questionary";
-import Questionarys from "./Questionarys";
-import Education from "./Education";
-import PreRegistration from "./PreRegistration";
-import Login from "./Login";
-import Missing from "./Missing";
-import Unauthorized from "./Unauthorized";
-import { Route, Routes } from "react-router-dom";
-import { DataProvider } from "../context/dataContext";
-import { AuthProvider } from "../context/AuthProvider";
-import RequireAuth from "./RequireAuth";
-import Registration from "./Registration";
+import Home from './Home';
+import Layout from './Layout';
+import Users from './Users';
+import User from './User';
+import Questionary from './Questionary';
+import Questionarys from './Questionarys';
+import Education from './Education';
+import PreRegistration from './PreRegistration';
+import Login from './Login';
+import Missing from './Missing';
+import Unauthorized from './Unauthorized';
+import { Route, Routes } from 'react-router-dom';
+import { DataProvider } from '../context/dataContext';
+import { AuthProvider } from '../context/AuthProvider';
+import RequireAuth from './RequireAuth';
+import Registration from './Registration';
 import LevelChanger from './LevelChanger';
 import Level from './Level';
 import EducationChanger from './EducationChanger';
@@ -25,6 +26,10 @@ function App() {
     <AuthProvider>
       <DataProvider>
         <Routes>
+          {/* public routes */}
+          <Route path="Login" element={<Login />} />
+          <Route path="unauthorized" element={<Unauthorized />} />
+          <Route path="*" element={<Missing />} />
           {/* we want to protect these routes */}
           <Route path="/" element={<Layout />}>
             <Route
@@ -45,18 +50,15 @@ function App() {
                 <Route path=":id" element={<Questionary />} />
               </Route>
               <Route path="Education">
-                <Route index element={<Education />} />
+                <Route index element={<Education urlPart={"Education"}/>} />
+                <Route path=":id" element={<Level/>} />
               </Route>
-
+              <Route path="Educationchanger">
+                <Route index element={<EducationChanger urlPart={"Educationchanger"}/>} />
+                <Route path=":id" element={<LevelChanger/>} />
+              </Route>
             </Route>
           </Route>
-
-          {/* </Route>
-          </Route> */}
-          {/* public routes */}
-          <Route path="Login" element={<Login />} />
-          <Route path="unauthorized" element={<Unauthorized />} />
-          <Route path="*" element={<Missing />} />
         </Routes>
       </DataProvider>
     </AuthProvider>
--- VandasPage/frontend/src/components/Nav.jsx
+++ VandasPage/frontend/src/components/Nav.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { Link } from "react-router-dom";
 import { useContext } from "react";
 import DataContext from "../context/dataContext";
