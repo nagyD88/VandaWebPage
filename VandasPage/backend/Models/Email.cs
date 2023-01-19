5f22350669bee2b81adab8bda8f7bbbe3ce66b49
nagyD88 <oliverdikker@gmail.com>
2023-01-19 17:32:33 +0100

    create modell for email

---
+++ VandasPage/backend/Models/Email.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+namespace VandasPage.Models
+{
+    public class Email
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long Id { get; set; }
+        public string name { get; set } = string.Empty;
+        public string To { get; set; } = string.Empty;
+        public string Subject { get; set; } = string.Empty;
+        public string Body { get; set; } = string.Empty;
+    }
+}
