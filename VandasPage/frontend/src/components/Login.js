bc51337ba8e8af64cf14b6a3b741a799177dc9be
nagyD88 <oliverdikker@gmail.com>
2023-01-19 15:10:53 +0100

    auth contains ID

--- VandasPage/frontend/src/components/Login.js
+++ VandasPage/frontend/src/components/Login.js
@@ -41,7 +41,8 @@ const Login = () => {
       //console.log(JSON.stringify(response));
       //const accessToken = response?.data?.accessToken;
       const admin = response?.data?.admin;
-      setAuth({ user: email, pwd: password, admin: admin/*, accessToken*/ });
+      const id = response?.data?.id;
+      setAuth({ user: email, pwd: password, admin: admin, id: id/*, accessToken*/ });
       setUser('');
       setPwd('');
       navigate(from, { replace: true });
