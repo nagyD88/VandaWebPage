a12e39cb5b0dc8f69d4f675bef976d747d9d4e23
nagyD88 <oliverdikker@gmail.com>
2023-01-20 17:16:05 +0100

    user gets pasword hash parts

--- VandasPage/backend/Models/User.cs
+++ VandasPage/backend/Models/User.cs
@@ -26,6 +26,11 @@ namespace VandasPage.Models
         public int? NumberOfDetailsLeft { get; set; }
         public decimal? priceLeft { get; set; }
         public string? Password { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+        public string RefreshToken { get; set; } = string.Empty;
+        public DateTime TokenCreated { get; set; }
+        public DateTime TokenExpires { get; set; }
         public HashSet<Level> Levels { get; set; }
 
     }
