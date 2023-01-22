93fecfc89b2e95e8c5988f67fa27cf05bdd94ce1
nagyD88 <oliverdikker@gmail.com>
2023-01-22 15:33:20 +0100

    paramters get string.empty default

--- VandasPage/backend/Models/User.cs
+++ VandasPage/backend/Models/User.cs
@@ -11,24 +11,24 @@ namespace VandasPage.Models
         [Required]
         public string Email { get; set; }
         public bool Admin { get; set; }
-        public string UserName { get; set; }
+        public string UserName { get; set; }=string.Empty;  
         [Required]
         public string FirstName { get; set; }
         [Required]
         public string LastName { get; set; }
-        public string? ReasonForApplication { get; set; }
+        public string ReasonForApplication { get; set; }=string.Empty;
         public bool? DirectInquiry { get; set; }
-        public string? Communication { get; set; }
+        public string Communication { get; set; }=string.Empty;
         private List<Questionnaire>? _questionnaires;
-        public string? MBTI { get; set; }
+        public string MBTI { get; set; }=String.Empty;
         public decimal? Price { get; set; }
         public int? NumberOfDetailsStart { get; set; }
         public HashSet<MeetingLog> MeetingLogs { get; set; }
         public int? NumberOfDetailsLeft { get; set; }
         public decimal? priceLeft { get; set; }
-        public byte[] PasswordHash { get; set; }
-        public byte[] PasswordSalt { get; set; }
-        public string RefreshToken { get; set; } = string.Empty;
+        public byte[]? PasswordHash { get; set; }
+        public byte[]? PasswordSalt { get; set; }
+        public string RefreshToken { get; set; } =string.Empty;
         public DateTime TokenCreated { get; set; }
         public DateTime TokenExpires { get; set; }
         public HashSet<Level> Levels { get; set; }
