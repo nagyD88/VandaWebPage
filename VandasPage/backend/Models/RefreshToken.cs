f9d3dd25f626cc305c37b7503efbba03ed09c6d7
nagyD88 <oliverdikker@gmail.com>
2023-01-22 14:27:48 +0100

    create paramater Id  user

--- VandasPage/backend/Models/RefreshToken.cs
+++ VandasPage/backend/Models/RefreshToken.cs
@@ -1,7 +1,12 @@
-﻿namespace VandasPage.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace VandasPage.Models
 { 
     public class RefreshToken
     {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long Id { get; set; }
+        public User User { get; set; }
         public string Token { get; set; } = string.Empty;
         public DateTime Created { get; set; } = DateTime.Now;
         public DateTime Expires { get; set; }
