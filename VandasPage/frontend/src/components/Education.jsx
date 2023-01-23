2cdc9630c1ade269d7fd2f671c9967c90041d76d
Beelzseals <88729970+Beelzseals@users.noreply.github.com>
2023-01-23 19:08:34 +0100

    -move course list under Education.js -make course list dynamic

--- VandasPage/frontend/src/components/Education.jsx
+++ VandasPage/frontend/src/components/Education.jsx
@@ -1,37 +1,37 @@
-import React from 'react';
-import useAxiosFetch from '../hooks/useAxiosFetch';
-import dataContext from '../context/dataContext';
-import { useContext, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import useAxiosFetch from "../hooks/useAxiosFetch";
+import dataContext from "../context/dataContext";
+import { useContext, useEffect } from "react";
+import { Link } from "react-router-dom";
 
-
-
-
-const Education = ({urlPart}) => {
-  let url = 'https://localhost:7168/api/Education/level';
+const Education = ({ urlPart }) => {
+  let url = "https://localhost:7168/api/Education/level";
   const { data, fetchError, isLoading } = useAxiosFetch(url);
   const { colorTheme, counter } = useContext(dataContext);
-  
+
   console.log(data);
 
   const categorys = new Set();
 
   useEffect(() => {
     data.map((level) => categorys.add(level.categoryName));
-    console.log('category');
+    console.log("category");
     console.log(categorys);
   }, [data, counter]);
   return (
     <>
+      <ul className="course-list">
       {data.map((level) => (
-        <Link key={level.id} to={`/${urlPart}/${level.id}`}>
-          <div className="level">
-            <p>
-              {level.name} 
-            </p>
-          </div>
-        </Link>
+        <li>
+          <Link key={level.id} to={`/Education/${level.id}`}>
+            {level.name}
+          </Link>
+        </li>
       ))}
+      </ul>
+      <button className="btn">New course</button>
+      <button className="btn">New lesson</button>
+      
     </>
   );
 };
