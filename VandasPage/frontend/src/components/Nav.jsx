843193943a696d16b8788bf289f54ca72de3a361
Beelzseals <88729970+Beelzseals@users.noreply.github.com>
2023-01-23 16:29:27 +0100

    fix navbar size

--- VandasPage/frontend/src/components/Nav.jsx
+++ VandasPage/frontend/src/components/Nav.jsx
@@ -6,26 +6,30 @@ import AuthContext from "../context/AuthProvider";
 
 const Nav = () => {
   const { colorTheme } = useContext(DataContext);
-  const {auth} = useContext(AuthContext)
-return (
-    <nav className={`sideNav ${colorTheme}`} >
-      <div className={`navElement ${colorTheme}`}>
-        <Link to="/">Home</Link>
-      </div>
-      {auth.admin && <>
-      <div className={`navElement ${colorTheme}`}>
-        <Link to="preregister">Előregisztráció</Link>
-      </div>
-      <div className={`navElement ${colorTheme}`}>
-        <Link to="register">Regisztráció</Link>
-      </div>
-      <div className={`navElement ${colorTheme}`}>
-        <Link to="User">Felhasználók</Link>
-      </div>
-      <div className={`navElement ${colorTheme}`}>
-        <Link to="Educationchanger">Oktás Változtatás</Link>
-      </div>
-      </>}
+  const { auth } = useContext(AuthContext);
+  return (
+    <nav className="navbar">
+      <p>Move cursor here for the navbar to appear</p>
+      <ul className="navbar-list">
+        <li className={`navElement ${colorTheme}`}>
+          <Link to="/">Home</Link>
+        </li>
+        {auth.admin && (
+          <>
+            <li className={`navElement ${colorTheme}`}>
+              <Link to="preregister">Előregisztráció</Link>
+            </li>
+            <li className={`navElement ${colorTheme}`}>
+              <Link to="register">Regisztráció</Link>
+            </li>
+            <li className={`navElement ${colorTheme}`}>
+              <Link to="User">Felhasználók</Link>
+            </li>
+            <li className={`navElement ${colorTheme}`}>
+              <Link to="Educationchanger">Oktás Változtatás</Link>
+            </li>
+          </>
+        )}
 
         <li className={`navElement ${colorTheme}`}>
           <Link to="questionnaire">Kérdőív</Link>
@@ -33,6 +37,7 @@ return (
         <li className={`navElement ${colorTheme}`}>
           <Link to="Education">Oktató felület</Link>
         </li>
+      </ul>
     </nav>
   );
 };
