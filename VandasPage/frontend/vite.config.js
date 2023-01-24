bdd5aa162ceebba9fd73cac234376b151469eea9
nagyD88 <oliverdikker@gmail.com>
2023-01-24 13:49:42 +0100

    fix server location

--- VandasPage/frontend/vite.config.js
+++ VandasPage/frontend/vite.config.js
@@ -1,7 +1,9 @@
 
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dns from 'dns'
 
+dns.setDefaultResultOrder('verbatim')
 export default defineConfig({
   plugins: [react()],
   server: {
