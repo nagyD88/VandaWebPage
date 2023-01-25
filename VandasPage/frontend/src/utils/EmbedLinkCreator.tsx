3d69a1c8d780a343bada3a996466eacdafc61cb3
nagyD88 <94189139+nagyD88@users.noreply.github.com>
2023-01-25 14:09:12 +0100

    implement types

--- VandasPage/frontend/src/utils/EmbedLinkCreator.tsx
+++ VandasPage/frontend/src/utils/EmbedLinkCreator.tsx
@@ -1,11 +1,7 @@
+export function getId(url: string): string | null {
+  const regExp: RegExp =
+    /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=)([^#&?]*).*/;
+  const match = url.match(regExp);
 
-
-
-export function getId(url) {
-    const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=)([^#&?]*).*/;
-    const match = url.match(regExp);
-
-    return (match && match[2].length === 11)
-      ? match[2]
-      : null;
+  return match && match[2].length === 11 ? match[2] : null;
 }
