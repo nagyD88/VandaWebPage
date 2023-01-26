853b0e53a0b2f444e72688c2f6ea17e05149d358
nagyD88 <94189139+nagyD88@users.noreply.github.com>
2023-01-26 12:10:54 +0100

    fix context

--- VandasPage/frontend/src/context/dataContext.tsx
+++ VandasPage/frontend/src/context/dataContext.tsx
@@ -1,19 +1,28 @@
-import React from "react";
-import { createContext, useState } from "react";
+import React, { ReactElement } from 'react';
+import { createContext, useState } from 'react';
 
-const DataContext = createContext({});
+type ChildrenType = { children?: ReactElement | ReactElement[] };
+type ColorTheme = "Light" | "Dark"
+const [colorTheme, setColorTheme] = useState<ColorTheme>('Light');
+const [counter, setCounter] = useState<number>(0);
+const DataContext = createContext({colorTheme, setColorTheme, counter, setCounter});
 
-export const DataProvider = ({children})=>{
-    
-    const [colorTheme, setColorTheme]= useState('Light');
-    const [counter, setCounter]=useState(0);
-    return (
-        <DataContext.Provider value={{
-            colorTheme, setColorTheme, counter, setCounter
-            }}>
-            {children}
-        </DataContext.Provider>
-    )
-} 
+
+export const DataProvider = ({ children }: ChildrenType): ReactElement => {
+
+
+  return (
+    <DataContext.Provider
+      value={{
+        colorTheme,
+        setColorTheme,
+        counter,
+        setCounter,
+      }}
+    >
+      {children}
+    </DataContext.Provider>
+  );
+};
 
 export default DataContext;
