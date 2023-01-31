d7653f547ec9ba77f041991ac3e3b6ff4fc28f9f
nagyD88 <94189139+nagyD88@users.noreply.github.com>
2023-01-31 08:22:21 +0100

    footer gets the current year

--- VandasPage/frontend/src/components/Footer.tsx
+++ VandasPage/frontend/src/components/Footer.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
-
+const date = new Date().getFullYear();
 const Footer = () => {
   return (
     <footer>
-      <p>&copy;Copyright 2023</p>
+      <p>&copy;Copyright {date}</p>
     </footer>
   )
 }
