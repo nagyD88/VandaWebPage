1c1c749c9a8bf1678cdc17f35e91214aeecdb851
nagyD88 <94189139+nagyD88@users.noreply.github.com>
2023-02-02 09:45:22 +0100

    fix naming

--- VandasPage/backend/Models/Email.cs
+++ VandasPage/backend/Models/Email.cs
@@ -5,7 +5,7 @@ namespace VandasPage.Models
     {
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public long Id { get; set; }
-        public string name { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
         public string To { get; set; } = string.Empty;
         public string Subject { get; set; } = string.Empty;
         public string Body { get; set; } = string.Empty;
