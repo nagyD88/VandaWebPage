b358d3dc3eacac62e76ececcdd9ac4742b811663
Beelzseals <88729970+Beelzseals@users.noreply.github.com>
2023-02-11 14:55:16 +0100

    disable tailwind preflight

--- VandasPage/frontend/tailwind.config.js
+++ VandasPage/frontend/tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  corePlugins: {preflight: false},
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
