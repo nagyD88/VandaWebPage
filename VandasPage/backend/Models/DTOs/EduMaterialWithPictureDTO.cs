fec43314e54ed61c01d16b3955b3932dd45e04f1
nagyD88 <94189139+nagyD88@users.noreply.github.com>
2023-02-15 08:35:29 +0100

    create DTO for EduMaterialWithPictureDTO

---
+++ VandasPage/backend/Models/DTOs/EduMaterialWithPictureDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace VandasPage.Models.DTOs
+{
+    public class EduMaterialWithPictureDTO
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public IFormFile File { get; set; }
+    }
+}
